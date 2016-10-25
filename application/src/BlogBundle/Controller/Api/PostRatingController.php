00204180d66db4777e6f31c2f133e5d3efa4b553
myrkox <berlad.myr@gmail.com>
2016-10-26 00:55:33 +0300

    SAB: base controller structure

---
+++ application/src/BlogBundle/Controller/Api/PostRatingController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BlogBundle\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class PostRatingController extends Controller
+{
+    public function getAction()
+    {
+        return $this->render('BlogBundle:Default:index.html.twig');
+    }
+
+    public function getAll()
+    {
+        return $this->render('BlogBundle:Default:index.html.twig');
+    }
+
+    public function create(Request $request)
+    {
+        return $this->render('BlogBundle:Default:index.html.twig');
+    }
+
+    public function update(Request $request)
+    {
+        return $this->render('BlogBundle:Default:index.html.twig');
+    }
+}
